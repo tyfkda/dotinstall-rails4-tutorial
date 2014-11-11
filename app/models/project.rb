28665fd1816789c8c90d5e8598e2dc135afbf529
tyfkda <tyfkda@gmail.com>
2014-11-12 08:58:41 +0900

    バリデーションに失敗した時にエラーメッセージを表示
    15 エラーメッセージを表示しよう
    http://dotinstall.com/lessons/basic_rails_v2/24915

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
 class Project < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: { message: '入力して下さい' },
+                    length: { minimum: 3, message: '短すぎ！' }
 end
