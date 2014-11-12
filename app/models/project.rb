8e7c8b9b199bafbc06d603f42f0a086091dd08e9
tyfkda <tyfkda@gmail.com>
2014-11-12 09:35:00 +0900

    ProjectがTaskと１対多であることを記述する
    Taskの方は生成時に自動的に「belongs_to :project」という指定が入っているが
    Projectの方は自動的には追加されないので、してやる必要がある。
    
    21 Associationの設定をしよう
    http://dotinstall.com/lessons/basic_rails_v2/24921

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 class Project < ActiveRecord::Base
+  has_many :tasks
   validates :title, presence: { message: '入力して下さい' },
                     length: { minimum: 3, message: '短すぎ！' }
 end
